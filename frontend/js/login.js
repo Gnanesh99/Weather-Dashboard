1df2242e1755cc207c5cfb66594fd5d4d32bf4c9
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2026-03-22 10:55:52 +0530

    login pages updated

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -22,7 +22,7 @@ if(data.token){
 localStorage.setItem("token",data.token);
 localStorage.setItem("user",JSON.stringify(data.user));
 
-window.location.href="dashboard.html";
+window.location.href="/dashboard.html";
 
 }else{
 
