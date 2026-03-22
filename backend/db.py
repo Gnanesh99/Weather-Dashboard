cbc9a5060e00cad465558af442f5d66abdef5183
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2026-03-22 13:36:12 +0530

    Database error test

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,12 @@
 import mysql.connector
+import os
 
 def get_db():
-
     db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="#Harrypotterfan1234",
-        database="weather_dashboard"
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306))
     )
-
     return db
