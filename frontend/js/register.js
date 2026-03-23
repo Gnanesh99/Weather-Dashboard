d6e04b90002a6b1ec78bf58eaae60aab32e6eb70
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2026-03-23 10:22:59 +0530

    register page update

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,10 +1,14 @@
 async function register(){
 
+console.log("REGISTER CLICKED");  // 🔥 add this
+
 const username = document.getElementById("username").value;
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 
 try{
+
+console.log("Sending request...");  // 🔥
 
 const response = await fetch("/api/auth/register", {
 method:"POST",
@@ -12,7 +16,10 @@ headers:{ "Content-Type":"application/json" },
 body: JSON.stringify({ username, email, password })
 });
 
+console.log("Response received:", response);  // 🔥
+
 const data = await response.json();
+console.log("Data:", data);  // 🔥
 
 if(response.ok){
 alert("Registered successfully");
@@ -22,7 +29,7 @@ alert(data.message);
 }
 
 }catch(err){
-console.error(err);
+console.error("ERROR:", err);
 alert("Register failed");
 }
 
